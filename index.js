4cd49bd466c3d5435f484129445a5c15d8b418ac
David Biddle <dbiddle@zonedigital.com>
2019-10-09 16:48:48 +0100

    readd image generation

--- index.js
+++ index.js
@@ -2,42 +2,37 @@ const fetch = require("isomorphic-fetch");
 
 exports.handler = async (event, context) => {
     if (new Date().getDay() === 3) {
+        const urls = ["frog1.jpg", "frog2.png"];
+        const number = Math.floor(Math.random() * urls.length);
+        // const mimetype = urls[number].includes(".jpg") ? "image/jpg" : "image/png";
+        const fileURL = `https://it-is-wednesday-my-dudes.netlify.com/img/${urls[number]}`;
+
+        let image;
+        try {
+            const result = await fetch(fileURL);
+            image = await result.buffer();
+        } catch (error) {
+            console.log("error", error);
+            return {
+                statusCode: 500,
+                body: JSON.stringify({
+                    error: error.message
+                })
+            };
+        }
+
         return {
             statusCode: 200,
-            body: "wednesday"
+            headers: {
+                "Content-type": image.type
+            },
+            body: image.toString("base64"),
+            isBase64Encoded: true
         };
-    }else{
-        
+    } else {
         return {
             statusCode: 404,
             body: "404 wednesday not found"
         };
     }
-    const urls = ["frog1.jpg", "frog2.png"];
-    const number = Math.floor(Math.random() * urls.length);
-    const mimetype = urls[number].includes(".jpg") ? "image/jpg" : "image/png";
-    const fileURL = `https://it-is-wednesday-my-dudes.netlify.com/img/${urls[number]}`;
-
-    let image;
-    try {
-        const result = await fetch(fileURL);
-        image = await result.buffer();
-    } catch (error) {
-        console.log("error", error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                error: error.message
-            })
-        };
-    }
-
-    return {
-        statusCode: 200,
-        headers: {
-            "Content-type": image.type
-        },
-        body: image.toString("base64"),
-        isBase64Encoded: true
-    };
 };
