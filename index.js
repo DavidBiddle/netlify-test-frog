b57c398ef6a061e2ff4c01a77903c6718b438198
David Biddle <dbiddle@zonedigital.com>
2019-10-09 15:13:30 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const fetch = require("isomorphic-fetch");
+
+exports.handler = async (event, context) => {
+    const { fileURL } = "./img/frog.jpg"
+
+    let image;
+    try {
+        const result = await fetch(fileURL);
+        image = await result.buffer();
+    } catch (error) {
+        console.log("error", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: error.message
+            })
+        };
+    }
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-type": "image/jpeg"
+        },
+        body: image.toString("base64"),
+        isBase64Encoded: true
+    };
+};
