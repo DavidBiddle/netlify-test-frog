52b4a5f9ae4f59dd74a9afdc66b21fbf2a562826
David Biddle <dbiddle@zonedigital.com>
2019-10-09 18:04:45 +0100

    add more imgs and some logic

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const fetch = require("isomorphic-fetch");
 
+const getURL = (urls, number) =>{
+    const today = new Date();
+    if ((today.getDate === 30 || today.getDate === 31) && today.getMonth === 9){
+        return 'specialimg/halloween.jpg';
+    }else{
+        return urls[number];
+    }
+}
+
 exports.handler = async (event, context) => {
     if (new Date().getDay() === 3) {
-        const urls = ["frog1.jpg", "frog2.png"];
+        const urls = [
+            "img/frog1.jpg",
+            "img/frog2.png",
+            "img/frog3.jpg",
+            "img/frog4.png",
+            "img/frog5.jpg",
+            "img/frog6.png",
+            "img/frog7.jpg",
+            "img/frog8.png",
+            "img/frog9.jpg",
+            "img/frog10.png",
+            "img/frog11.jpg",
+            "img/frog12.jpg",
+            "img/frog13.jpg",
+            "img/frog14.jpg",
+            "img/frog15.png",
+            "img/frog16.png"
+        ];
         const number = Math.floor(Math.random() * urls.length);
+        const url = getURL(urls,number);
         // const mimetype = urls[number].includes(".jpg") ? "image/jpg" : "image/png";
-        const fileURL = `https://it-is-wednesday-my-dudes.netlify.com/img/${urls[number]}`;
+        const fileURL = `https://it-is-wednesday-my-dudes.netlify.com/${url}`;
 
         let image;
         try {
